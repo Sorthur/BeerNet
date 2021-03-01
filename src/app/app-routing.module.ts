861f6873bc15f72bc95174e74dd303a83fda73fa
Mateusz Dejewski <matdejewski@gmail.com>
2021-03-01 15:28:04 +0100

    link search & search-beer components in Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { SearchBeerComponent } from './search-beer/search-beer.component';
+import { SearchComponent } from './search/search.component';
 import { UserAccountComponent } from './user-account/user-account.component';
 
 const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'myprofile', component: UserAccountComponent },
+    { path: 'search', component: SearchComponent, children: [{ path: 'beer', component: SearchBeerComponent }] },
 ];
 
 @NgModule({
