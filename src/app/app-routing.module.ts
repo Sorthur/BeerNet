3eb61718a9a870426b3e2e36211499efdac599ac
Mateusz Dejewski <matdejewski@gmail.com>
2021-03-01 14:51:58 +0100

    link user-account.component in Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { UserAccountComponent } from './user-account/user-account.component';
 
 const routes: Routes = [
     { path: 'login', component: LoginComponent },
+    { path: 'myprofile', component: UserAccountComponent },
 ];
 
 @NgModule({
