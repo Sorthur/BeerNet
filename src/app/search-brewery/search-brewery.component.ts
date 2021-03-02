2eb40e091749270064309e18cfa7f2e0bd3d64b9
Mateusz Dejewski <matdejewski@gmail.com>
2021-03-02 14:46:36 +0100

    add content to search-brewery

--- src/app/search-brewery/search-brewery.component.ts
+++ src/app/search-brewery/search-brewery.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApiService } from '../api.service';
+import { BreweryModel } from '../models/breweryModel';
+import { Country } from '../models/enums/country';
+import { BreweryFilter } from '../models/filters/breweryFilter';
 
 @Component({
-  selector: 'app-search-brewery',
-  templateUrl: './search-brewery.component.html',
-  styleUrls: ['./search-brewery.component.css']
+    selector: 'app-search-brewery',
+    templateUrl: './search-brewery.component.html',
+    styleUrls: ['./search-brewery.component.css']
 })
 export class SearchBreweryComponent implements OnInit {
+    declare breweryModels: BreweryModel[];
+    countryEnum = Country;
+    countryKeys: any[];
+    constructor(private formBuilder: FormBuilder, private api: ApiService) {
+        // Object.values() returns enum's values and keys, so we remove half of the array that contains keys
+        this.countryKeys = Object.values(this.countryEnum);
+        this.countryKeys = this.countryKeys.slice(0, this.countryKeys.length / 2);
+    }
 
-  constructor() { }
+    searchBreweryForm: FormGroup = this.formBuilder.group({
+        breweryName: '',
+        country: ''
+    });
 
-  ngOnInit(): void {
-  }
+    async searchBreweries() {
+        try {
+            await this.api.getBreweries(new BreweryFilter(
+                this.searchBreweryForm.value.breweryName,
+                this.searchBreweryForm.value.country
+            )).then(data => {
+                this.breweryModels = data;
+            });
+        }
+        catch (error) {
+            console.log(error);
+            return;
+        }
+    }
 
+    ngOnInit(): void { }
 }
