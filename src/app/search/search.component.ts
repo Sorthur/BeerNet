1154f47b3a89f5bed2a2fe3b544e20f24726a541
Mateusz Dejewski <matdejewski@gmail.com>
2021-03-03 11:21:20 +0100

    search.component checks which tab is active

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,13 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-search',
     templateUrl: './search.component.html',
     styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, DoCheck {
     activeTab: number = 0;
-    constructor() { }
+    constructor(private route: ActivatedRoute) { }
+
+    ngDoCheck(): void {
+        this.route.url.subscribe(() => {
+            let childUrlName = this.route.snapshot.firstChild?.url[0].path;
+            console.log(`childUrlName: ${childUrlName}`)
+            if (childUrlName === "beer") {
+                this.activeTab = 0;
+            } else if (childUrlName === "brewery") {
+                this.activeTab = 1;
+            } else {
+                this.activeTab = 2;
+            }
+        }).unsubscribe();
+    }
 
     ngOnInit(): void { }
 }
