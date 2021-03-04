28f5821f64eacea2ae0829aefe9d40891ff4ebb8
Mateusz Dejewski <matdejewski@gmail.com>
2021-03-04 16:49:31 +0100

    add working form to register component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AppSettings } from 'src/AppSettings';
+import { ApiService } from '../api.service';
+import { RegisterModel } from '../models/registerModel';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+    errorLabel: string = '';
+    constructor(private formBuilder: FormBuilder, private api: ApiService, private router: Router) { }
 
-  constructor() { }
+    registerForm: FormGroup = this.formBuilder.group({
+        login: '',
+        email: '',
+        password: ''
+    });
 
-  ngOnInit(): void {
-  }
+    async register(): Promise<void> {
+        this.errorLabel = '';
+        try {
+            await this.api.registerUser(new RegisterModel(
+                this.registerForm.value.login,
+                this.registerForm.value.email,
+                this.registerForm.value.password
+            ));
+        }
+        catch (error) {
+            if (error.status == 409) {
+                this.errorLabel = "User already exists";
+                return;
+            }
+            console.log(error);
+        }
+        window.alert("User registered successfully")
+        this.router.navigate(['/']);
+    }
 
+    ngOnInit(): void { }
 }
