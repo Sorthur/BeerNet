b5bbbd01348af5eb361290030363b76c83dd78f3
Mateusz Dejewski <matdejewski@gmail.com>
2021-03-04 16:46:47 +0100

    link register component in Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { BeerComponent } from './beer/beer.component';
 import { BreweryComponent } from './brewery/brewery.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { SearchBeerComponent } from './search-beer/search-beer.component';
 import { SearchBreweryComponent } from './search-brewery/search-brewery.component';
 import { SearchComponent } from './search/search.component';
@@ -10,6 +11,7 @@ import { UserAccountComponent } from './user-account/user-account.component';
 
 const routes: Routes = [
     { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
     { path: 'myprofile', component: UserAccountComponent },
     { path: 'search', component: SearchComponent, children: [{ path: 'beer', component: SearchBeerComponent }, { path: 'brewery', component: SearchBreweryComponent }] },
     { path: 'beer/:id', component: BeerComponent },
